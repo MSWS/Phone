6cc90b0a9c2e57c9f25adc35c435d50f36a3643e
MSWS <imodmaker@gmail.com>
2023-11-05 15:58:41 -0800

    Customize webhook

--- src/main/java/xyz/msws/phone/Main.java
+++ src/main/java/xyz/msws/phone/Main.java
@@ -9,8 +9,9 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) throws ServletException {
         final String token = System.getenv("DISCORD_TOKEN");
+        final String webhook = System.getenv("TWILIO_WEBHOOK");
         IPhoneBot bot = new PhoneBot(token);
 
-        new TwilioListener(bot);
+        new TwilioListener(bot, webhook);
     }
 }
--- src/main/java/xyz/msws/phone/TwilioListener.java
+++ src/main/java/xyz/msws/phone/TwilioListener.java
@@ -10,12 +10,12 @@ public class TwilioListener {
 
     private IPhoneBot bot;
 
-    public TwilioListener(IPhoneBot bot) {
+    public TwilioListener(IPhoneBot bot, String webhook) {
         this.bot = bot;
 
-        get("/", (req, res) -> "Hello Web");
+//        get("/", (req, res) -> "Hello Web");
 
-        post("/sms", (req, res) -> {
+        post("/" + webhook, (req, res) -> {
             String body = req.queryParams("Body");
             String from = req.queryParams("From");
 
